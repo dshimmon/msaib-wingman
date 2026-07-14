bd4fdb822da0129ef50781f52fe9553382ede5c2
David Shimmon <dshimmon16@gmail.com>
2026-07-14 13:51:01 -0400

    Refashioned code in main.py and topics.json

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 # MSAIB Wingman
 # Main application entry point
 
+import json
+
 print("=" * 40)
 print("        MSAIB WINGMAN")
 print("=" * 40)
@@ -12,15 +14,16 @@ print()
 mission = input("What is today's mission? ")
 normalized_mission = mission.lower()
 
-topics = {"python": "Programming", "regression": "Managerial Statistics"}
+with open("data/topics.json", "r") as file:
+    topics = json.load(file)
 
 print()
 
+print(f"Topic identified: {mission}")
+
 if normalized_mission in topics:
-    print(f"Topic identified: {mission}")
     print(f"Likely course: {topics[normalized_mission]}")
 else:
-    print(f"Topic identified: {mission}")
     print("Course unknown.")
     print("Future versions of Wingman will search all course materials.")
 
